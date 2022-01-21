b04e06beab06cac715d4666d74b1f20b36837ff5
Joshua Miles <joshmilesproduction@gmail.com>
2022-01-21 16:06:55 +0000

    added basics

--- imthecatbot/imthecatbot/Program.cs
+++ imthecatbot/imthecatbot/Program.cs
@@ -1,18 +1,43 @@
 ﻿using System;
+using System.IO;
+using System.Net.Sockets;
 using System.Threading.Tasks;
+using AsyncAwaitBestPractices;
 
 namespace imthecatbot
 {
     class Program
     {
-        string ip = "irc.chat.twitch.tv";
-        int port = 6667;
-        string password = catSecrets.botPassword;
-        string botUsername = catSecrets.botUsername;
 
         static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Starting up.");
+
+            catBot meow = new catBot(catSecrets.botUsername, catSecrets.botPassword, catSecrets.ip, catSecrets.port, catSecrets.twitchChannel);
+
+            meow.loginBot().SafeFireAndForget();
+
+            Console.WriteLine("Started up.");
+
+            Console.WriteLine("Waking up...");
+
+            await meow.sendMessage("*yawns* I have woken up:)");
+
+            meow.OnMessage += async (sender, twitchChatMessage) =>
+            {
+                Console.WriteLine($"{twitchChatMessage.Sender} said {twitchChatMessage.Message}");
+
+                if (twitchChatMessage.Message.StartsWith("!meow"))
+                {
+                    await meow.sendMessage($"MEOW MEOW @{twitchChatMessage.Sender}! MEOWWWW");
+
+                }
+
+            };
+
+            await Task.Delay(-1);
+
         }
     }
 }
---
+++ imthecatbot/imthecatbot/catBot.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace imthecatbot
+{
+    public class catBot
+    {
+        StreamWriter sw;
+        StreamReader sr;
+        bool listening = true;
+
+        private string twitchChannel;
+        private string botUsername;
+        private string botPassword;
+        private string ip;
+        private int port;
+
+        private TaskCompletionSource<int> connected = new TaskCompletionSource<int>();
+
+        public event TwitchChatEventHandler OnMessage = delegate { };
+        public delegate void TwitchChatEventHandler(object sender, TwitchChatMessage e);
+
+        public class TwitchChatMessage : EventArgs
+        {
+            public string Sender { get; set; }
+            public string Message { get; set; }
+            public string Channel { get; set; }
+        }
+
+        public catBot(string botUsername, string botPassword, string ip, int port, string twitchChannel)
+        {
+            this.twitchChannel = twitchChannel;
+            this.botUsername = botUsername;
+            this.botPassword = botPassword;
+            this.ip = ip;
+            this.port = port;
+        }
+
+        public async Task loginBot()
+        {
+            Console.WriteLine("Attempting login.");
+
+
+            var tcpClient = new TcpClient();
+            await tcpClient.ConnectAsync(ip, port);
+
+            sr = new StreamReader(tcpClient.GetStream());
+            sw = new StreamWriter(tcpClient.GetStream())
+            {
+                NewLine = "\t\n",
+                AutoFlush = true
+            };
+
+            await sw.WriteLineAsync($"PASS {botPassword}");
+            await sw.WriteLineAsync($"NICK {botUsername}");
+            await sw.WriteLineAsync($"JOIN #{twitchChannel}");
+            connected.SetResult(0);
+
+            Console.WriteLine("Login Complete.");
+            Console.WriteLine("Listening...");
+
+            while (listening)
+            {
+                string line = await sr.ReadLineAsync();
+                Console.WriteLine(line);
+
+                string[] split = line.Split(" ");
+
+                if (line.StartsWith("PING"))
+                {
+                    Console.WriteLine("PONGing the PING");
+                    await sw.WriteLineAsync($"PONG {split[1]}");
+                }
+
+                if (split.Length > 1 && split[1] == "PRIVMSG")
+                {
+                    int exclamationPointPosition = split[0].IndexOf("!");
+                    string username = split[0].Substring(1, exclamationPointPosition - 1);
+                    int secondColonPosition = line.IndexOf(':', 1);
+                    string message = line.Substring(secondColonPosition + 1);
+                    string channel = split[2].TrimStart('#');
+
+                    OnMessage(this, new TwitchChatMessage
+                    {
+                        Message = message,
+                        Sender = username,
+                        Channel = channel
+                    });
+                }
+
+            }
+        }
+
+        public async Task sendMessage(string messageText)
+        {
+            await connected.Task;
+            await sw.WriteLineAsync($"PRIVMSG #{twitchChannel} :{messageText}");
+        }
+
+    }
+}
