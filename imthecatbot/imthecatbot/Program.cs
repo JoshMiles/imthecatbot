ed7760cfd9aea2fde3ae29a0961c57f67718beb9
Joshua Miles <joshmilesproduction@gmail.com>
2022-01-21 15:15:22 +0000

    we move

--- imthecatbot/imthecatbot/Program.cs
+++ imthecatbot/imthecatbot/Program.cs
@@ -1,10 +1,16 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace imthecatbot
 {
     class Program
     {
-        static void Main(string[] args)
+        string ip = "irc.chat.twitch.tv";
+        int port = 6667;
+        string password = catSecrets.botPassword;
+        string botUsername = catSecrets.botUsername;
+
+        static async Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
         }
